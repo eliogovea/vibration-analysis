3c30e58331645342f40474a2ee1c5f5d096a93f4
eliogovea <eliogovea1993@gmail.com>
2019-04-12 14:31:01 -0400

    +moc

---
+++ moc_reader.cpp
@@ -0,0 +1,214 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'reader.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.9.5)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "reader.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'reader.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.9.5. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_Reader_t {
+    QByteArrayData data[13];
+    char stringdata0[77];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_Reader_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_Reader_t qt_meta_stringdata_Reader = {
+    {
+QT_MOC_LITERAL(0, 0, 6), // "Reader"
+QT_MOC_LITERAL(1, 7, 8), // "newDataX"
+QT_MOC_LITERAL(2, 16, 0), // ""
+QT_MOC_LITERAL(3, 17, 1), // "x"
+QT_MOC_LITERAL(4, 19, 8), // "newDataY"
+QT_MOC_LITERAL(5, 28, 1), // "y"
+QT_MOC_LITERAL(6, 30, 8), // "newDataZ"
+QT_MOC_LITERAL(7, 39, 1), // "z"
+QT_MOC_LITERAL(8, 41, 7), // "newData"
+QT_MOC_LITERAL(9, 49, 7), // "AccData"
+QT_MOC_LITERAL(10, 57, 1), // "v"
+QT_MOC_LITERAL(11, 59, 11), // "readNewLine"
+QT_MOC_LITERAL(12, 71, 5) // "start"
+
+    },
+    "Reader\0newDataX\0\0x\0newDataY\0y\0newDataZ\0"
+    "z\0newData\0AccData\0v\0readNewLine\0start"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_Reader[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       6,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       4,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    1,   44,    2, 0x06 /* Public */,
+       4,    1,   47,    2, 0x06 /* Public */,
+       6,    1,   50,    2, 0x06 /* Public */,
+       8,    1,   53,    2, 0x06 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags
+      11,    0,   56,    2, 0x0a /* Public */,
+      12,    0,   57,    2, 0x0a /* Public */,
+
+ // signals: parameters
+    QMetaType::Void, QMetaType::Double,    3,
+    QMetaType::Void, QMetaType::Double,    5,
+    QMetaType::Void, QMetaType::Double,    7,
+    QMetaType::Void, 0x80000000 | 9,   10,
+
+ // slots: parameters
+    QMetaType::Bool,
+    QMetaType::Bool,
+
+       0        // eod
+};
+
+void Reader::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        Reader *_t = static_cast<Reader *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->newDataX((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 1: _t->newDataY((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 2: _t->newDataZ((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 3: _t->newData((*reinterpret_cast< AccData(*)>(_a[1]))); break;
+        case 4: { bool _r = _t->readNewLine();
+            if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = std::move(_r); }  break;
+        case 5: { bool _r = _t->start();
+            if (_a[0]) *reinterpret_cast< bool*>(_a[0]) = std::move(_r); }  break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 3:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< AccData >(); break;
+            }
+            break;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            typedef void (Reader::*_t)(double );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&Reader::newDataX)) {
+                *result = 0;
+                return;
+            }
+        }
+        {
+            typedef void (Reader::*_t)(double );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&Reader::newDataY)) {
+                *result = 1;
+                return;
+            }
+        }
+        {
+            typedef void (Reader::*_t)(double );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&Reader::newDataZ)) {
+                *result = 2;
+                return;
+            }
+        }
+        {
+            typedef void (Reader::*_t)(AccData );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&Reader::newData)) {
+                *result = 3;
+                return;
+            }
+        }
+    }
+}
+
+const QMetaObject Reader::staticMetaObject = {
+    { &QObject::staticMetaObject, qt_meta_stringdata_Reader.data,
+      qt_meta_data_Reader,  qt_static_metacall, nullptr, nullptr}
+};
+
+
+const QMetaObject *Reader::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Reader::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_Reader.stringdata0))
+        return static_cast<void*>(this);
+    return QObject::qt_metacast(_clname);
+}
+
+int Reader::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 6)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 6;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 6)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 6;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void Reader::newDataX(double _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+
+// SIGNAL 1
+void Reader::newDataY(double _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 1, _a);
+}
+
+// SIGNAL 2
+void Reader::newDataZ(double _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 2, _a);
+}
+
+// SIGNAL 3
+void Reader::newData(AccData _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 3, _a);
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
